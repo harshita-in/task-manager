5dee15908cfdfe9357bda89e21b7a28c40025633
harshita shrivastava <shrivastavaharshita88@gmail.com>
2026-02-03 11:23:28 +0530

    Enhance: Enable Firestore offline persistence

--- src/firebase.js
+++ src/firebase.js
@@ -1,6 +1,6 @@
 import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
-import { getFirestore } from 'firebase/firestore';
+import { getFirestore, enableIndexedDbPersistence } from 'firebase/firestore';
 
 const firebaseConfig = {
   apiKey: "AIzaSyDJH95aTTNdfduS7MOAuaMxuAAmhNDzReo",
@@ -15,3 +15,14 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const db = getFirestore(app);
+
+// Enable Offline Persistence
+enableIndexedDbPersistence(db).catch((err) => {
+  if (err.code == 'failed-precondition') {
+    // Multiple tabs open, persistence can only be enabled in one tab at a a time.
+    console.log("Persistence failed: Multiple tabs open");
+  } else if (err.code == 'unimplemented') {
+    // The current browser does not support all of the features required to enable persistence
+    console.log("Persistence failed: Browser not supported");
+  }
+});
